b19345ee9b4e7919ec1219f86c5c53bc5172e730
Omer Kala <kalaomer@hotmail.com>
2021-04-06 22:41:28 +0300

    ssr bug fix

--- dist/esnek.js
+++ dist/esnek.js
@@ -1,209 +1,30 @@
-'use strict';
+(function (vue, changeCase) {
+  'use strict';
 
-var changeCase = require('change-case');
-
-//
-//
-//
-//
-//
-//
-
-var script = {
-    name: "es-container",
-    props: {
-        gutter: {
-            default: undefined
-        },
+  var script = {
+      name: "es-container",
+      props: {
         fluid: Boolean
-    }
-};
+      }
+    };
 
-function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-    if (typeof shadowMode !== 'boolean') {
-        createInjectorSSR = createInjector;
-        createInjector = shadowMode;
-        shadowMode = false;
-    }
-    // Vue.extend constructor export interop.
-    const options = typeof script === 'function' ? script.options : script;
-    // render functions
-    if (template && template.render) {
-        options.render = template.render;
-        options.staticRenderFns = template.staticRenderFns;
-        options._compiled = true;
-        // functional template
-        if (isFunctionalTemplate) {
-            options.functional = true;
-        }
-    }
-    // scopedId
-    if (scopeId) {
-        options._scopeId = scopeId;
-    }
-    let hook;
-    if (moduleIdentifier) {
-        // server build
-        hook = function (context) {
-            // 2.3 injection
-            context =
-                context || // cached call
-                    (this.$vnode && this.$vnode.ssrContext) || // stateful
-                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
-            // 2.2 with runInNewContext: true
-            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-                context = __VUE_SSR_CONTEXT__;
-            }
-            // inject component styles
-            if (style) {
-                style.call(this, createInjectorSSR(context));
-            }
-            // register component module identifier for async chunk inference
-            if (context && context._registeredComponents) {
-                context._registeredComponents.add(moduleIdentifier);
-            }
-        };
-        // used by ssr in case component is cached and beforeCreate
-        // never gets called
-        options._ssrRegister = hook;
-    }
-    else if (style) {
-        hook = shadowMode
-            ? function (context) {
-                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
-            }
-            : function (context) {
-                style.call(this, createInjector(context));
-            };
-    }
-    if (hook) {
-        if (options.functional) {
-            // register for functional component in vue file
-            const originalRender = options.render;
-            options.render = function renderWithStyleInjection(h, context) {
-                hook.call(context);
-                return originalRender(h, context);
-            };
-        }
-        else {
-            // inject component registration as beforeCreate hook
-            const existing = options.beforeCreate;
-            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-    }
-    return script;
-}
+  const _withId = /*#__PURE__*/vue.withScopeId("data-v-19d8ec21");
 
-const isOldIE = typeof navigator !== 'undefined' &&
-    /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
-function createInjector(context) {
-    return (id, style) => addStyle(id, style);
-}
-let HEAD;
-const styles = {};
-function addStyle(id, css) {
-    const group = isOldIE ? css.media || 'default' : id;
-    const style = styles[group] || (styles[group] = { ids: new Set(), styles: [] });
-    if (!style.ids.has(id)) {
-        style.ids.add(id);
-        let code = css.source;
-        if (css.map) {
-            // https://developer.chrome.com/devtools/docs/javascript-debugging
-            // this makes source maps inside style tags work properly in Chrome
-            code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
-            // http://stackoverflow.com/a/26603875
-            code +=
-                '\n/*# sourceMappingURL=data:application/json;base64,' +
-                    btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
-                    ' */';
-        }
-        if (!style.element) {
-            style.element = document.createElement('style');
-            style.element.type = 'text/css';
-            if (css.media)
-                style.element.setAttribute('media', css.media);
-            if (HEAD === undefined) {
-                HEAD = document.head || document.getElementsByTagName('head')[0];
-            }
-            HEAD.appendChild(style.element);
-        }
-        if ('styleSheet' in style.element) {
-            style.styles.push(code);
-            style.element.styleSheet.cssText = style.styles
-                .filter(Boolean)
-                .join('\n');
-        }
-        else {
-            const index = style.ids.size - 1;
-            const textNode = document.createTextNode(code);
-            const nodes = style.element.childNodes;
-            if (nodes[index])
-                style.element.removeChild(nodes[index]);
-            if (nodes.length)
-                style.element.insertBefore(textNode, nodes[index]);
-            else
-                style.element.appendChild(textNode);
-        }
-    }
-}
+  const render = /*#__PURE__*/_withId((_ctx, _cache, $props, $setup, $data, $options) => {
+    return (vue.openBlock(), vue.createBlock("div", {
+      class: ["es-container container-fluid", {'container': ! $props.fluid}]
+    }, [
+      vue.renderSlot(_ctx.$slots, "default")
+    ], 2 /* CLASS */))
+  });
 
-/* script */
-const __vue_script__ = script;
+  script.render = render;
+  script.__scopeId = "data-v-19d8ec21";
+  script.__file = "src/components/container.vue";
 
-/* template */
-var __vue_render__ = function() {
-  var _vm = this;
-  var _h = _vm.$createElement;
-  var _c = _vm._self._c || _h;
-  return _c(
-    "div",
-    {
-      staticClass: "es-container container-fluid",
-      class: { container: !_vm.fluid }
-    },
-    [_vm._t("default")],
-    2
-  )
-};
-var __vue_staticRenderFns__ = [];
-__vue_render__._withStripped = true;
-
-  /* style */
-  const __vue_inject_styles__ = function (inject) {
-    if (!inject) return
-    inject("data-v-03f50598_0", { source: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", map: {"version":3,"sources":[],"names":[],"mappings":"","file":"container.vue"}, media: undefined });
-
-  };
-  /* scoped */
-  const __vue_scope_id__ = "data-v-03f50598";
-  /* module identifier */
-  const __vue_module_identifier__ = undefined;
-  /* functional template */
-  const __vue_is_functional_template__ = false;
-  /* style inject SSR */
-  
-  /* style inject shadow dom */
-  
-
-  
-  const __vue_component__ = normalizeComponent(
-    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
-    __vue_inject_styles__,
-    __vue_script__,
-    __vue_scope_id__,
-    __vue_is_functional_template__,
-    __vue_module_identifier__,
-    false,
-    createInjector,
-    undefined,
-    undefined
-  );
-
-//
-
-var script$1 = {
-    name: "es-row",
-    props: {
+  var script$1 = {
+      name: "es-row",
+      props: {
         col: [Number, String],
         noGutter: Boolean,
 
@@ -216,85 +37,48 @@ var script$1 = {
         justifyContentEnd: Boolean,
         justifyContentAround: Boolean,
         justifyContentBetween: Boolean,
-    },
-    computed: {
+      },
+      computed: {
         rowClasses() {
-            let classes = [];
+          let classes = [];
 
-            if (this.col) {
-                classes.push(`row-cols-${this.col}`);
+          if (this.col) {
+            classes.push(`row-cols-${this.col}`);
+          }
+
+          this.noGutter ? classes.push('no-gutters') : false;
+
+          const propClasses = ["alignContentStart", "alignContentCenter", "alignContentEnd",
+            "justifyContentStart", "justifyContentCenter", "justifyContentEnd", "justifyContentAround", "justifyContentBetween"];
+
+          for (let c of propClasses) {
+            if (this[c]) {
+              classes.push(changeCase.paramCase(c));
             }
+          }
 
-            this.noGutter ? classes.push('no-gutters'): false;
-
-            const propClasses = ["alignContentStart", "alignContentCenter", "alignContentEnd",
-                "justifyContentStart", "justifyContentCenter", "justifyContentEnd", "justifyContentAround", "justifyContentBetween"];
-
-            for (let c of propClasses) {
-                if (this[c]) {
-                    classes.push(changeCase.paramCase(c));
-                }
-            }
-
-            return classes
+          return classes
         }
-    }
-};
+      }
+    };
 
-/* script */
-const __vue_script__$1 = script$1;
+  const _withId$1 = /*#__PURE__*/vue.withScopeId("data-v-23fc6a0c");
 
-/* template */
-var __vue_render__$1 = function() {
-  var _vm = this;
-  var _h = _vm.$createElement;
-  var _c = _vm._self._c || _h;
-  return _c(
-    "div",
-    { staticClass: "es-row row", class: _vm.rowClasses },
-    [_vm._t("default")],
-    2
-  )
-};
-var __vue_staticRenderFns__$1 = [];
-__vue_render__$1._withStripped = true;
+  const render$1 = /*#__PURE__*/_withId$1((_ctx, _cache, $props, $setup, $data, $options) => {
+    return (vue.openBlock(), vue.createBlock("div", {
+      class: ["es-row row", $options.rowClasses]
+    }, [
+      vue.renderSlot(_ctx.$slots, "default")
+    ], 2 /* CLASS */))
+  });
 
-  /* style */
-  const __vue_inject_styles__$1 = function (inject) {
-    if (!inject) return
-    inject("data-v-c02248cc_0", { source: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", map: {"version":3,"sources":[],"names":[],"mappings":"","file":"row.vue"}, media: undefined });
+  script$1.render = render$1;
+  script$1.__scopeId = "data-v-23fc6a0c";
+  script$1.__file = "src/components/row.vue";
 
-  };
-  /* scoped */
-  const __vue_scope_id__$1 = "data-v-c02248cc";
-  /* module identifier */
-  const __vue_module_identifier__$1 = undefined;
-  /* functional template */
-  const __vue_is_functional_template__$1 = false;
-  /* style inject SSR */
-  
-  /* style inject shadow dom */
-  
-
-  
-  const __vue_component__$1 = normalizeComponent(
-    { render: __vue_render__$1, staticRenderFns: __vue_staticRenderFns__$1 },
-    __vue_inject_styles__$1,
-    __vue_script__$1,
-    __vue_scope_id__$1,
-    __vue_is_functional_template__$1,
-    __vue_module_identifier__$1,
-    false,
-    createInjector,
-    undefined,
-    undefined
-  );
-
-//
-
-var script$2 = {
-    name: "es-col",
-    props: {
+  var script$2 = {
+      name: "es-col",
+      props: {
         order: Number,
         alignSelfStart: Boolean,
         alignSelfCenter: Boolean,
@@ -306,99 +90,67 @@ var script$2 = {
         md: [Number, Boolean],
         sm: [Number, Boolean],
         xs: [Number, Boolean],
-    },
-    computed: {
+      },
+      computed: {
         colClasses() {
-            let classes = [];
+          let classes = [];
 
-            if (this.order) {
-                classes.push(`order-${this.order}`);
+          if (this.order) {
+            classes.push(`order-${this.order}`);
+          }
+
+          const propClasses = ["alignSelfStart", "alignSelfCenter", "alignSelfEnd"];
+
+          for (let c of propClasses) {
+            if (this[c]) {
+              classes.push(changeCase.paramCase(c));
             }
+          }
 
-            const propClasses = ["alignSelfStart", "alignSelfCenter", "alignSelfEnd"];
+          if (this.size) {
+            classes.push(`col-${this.size}`);
+          }
 
-            for (let c of propClasses) {
-                if (this[c]) {
-                    classes.push(changeCase.paramCase(c));
-                }
+          const propSizeClasses = ["xl", "lg", "md", "sm", "xs"];
+
+          for (let c of propSizeClasses) {
+            if (this[c]) {
+              if (this[c] === true) {
+                classes.push(`col-${c}`);
+              } else {
+                classes.push(`col-${c}-${this[c]}`);
+              }
             }
+          }
 
-            if (this.size) {
-                classes.push(`col-${this.size}`);
-            }
-
-            const propSizeClasses = ["xl", "lg", "md", "sm", "xs"];
-
-            for (let c of propSizeClasses) {
-                if (this[c]) {
-                    if (this[c] === true) {
-                        classes.push(`col-${c}`);
-                    } else {
-                        classes.push(`col-${c}-${this[c]}`);
-                    }
-                }
-            }
-
-            return classes;
+          return classes;
         }
-    }
-};
+      }
+    };
 
-/* script */
-const __vue_script__$2 = script$2;
+  const _withId$2 = /*#__PURE__*/vue.withScopeId("data-v-582d60c0");
 
-/* template */
-var __vue_render__$2 = function() {
-  var _vm = this;
-  var _h = _vm.$createElement;
-  var _c = _vm._self._c || _h;
-  return _c(
-    "div",
-    { staticClass: "es-col col", class: _vm.colClasses },
-    [_vm._t("default")],
-    2
-  )
-};
-var __vue_staticRenderFns__$2 = [];
-__vue_render__$2._withStripped = true;
+  const render$2 = /*#__PURE__*/_withId$2((_ctx, _cache, $props, $setup, $data, $options) => {
+    return (vue.openBlock(), vue.createBlock("div", {
+      class: ["es-col col", $options.colClasses]
+    }, [
+      vue.renderSlot(_ctx.$slots, "default")
+    ], 2 /* CLASS */))
+  });
 
-  /* style */
-  const __vue_inject_styles__$2 = function (inject) {
-    if (!inject) return
-    inject("data-v-0900f1ce_0", { source: "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", map: {"version":3,"sources":[],"names":[],"mappings":"","file":"col.vue"}, media: undefined });
+  script$2.render = render$2;
+  script$2.__scopeId = "data-v-582d60c0";
+  script$2.__file = "src/components/col.vue";
 
-  };
-  /* scoped */
-  const __vue_scope_id__$2 = "data-v-0900f1ce";
-  /* module identifier */
-  const __vue_module_identifier__$2 = undefined;
-  /* functional template */
-  const __vue_is_functional_template__$2 = false;
-  /* style inject SSR */
-  
-  /* style inject shadow dom */
-  
-
-  
-  const __vue_component__$2 = normalizeComponent(
-    { render: __vue_render__$2, staticRenderFns: __vue_staticRenderFns__$2 },
-    __vue_inject_styles__$2,
-    __vue_script__$2,
-    __vue_scope_id__$2,
-    __vue_is_functional_template__$2,
-    __vue_module_identifier__$2,
-    false,
-    createInjector,
-    undefined,
-    undefined
-  );
-
-const Esnek = {
+  const Esnek = {
     install(Vue, options) {
-        Vue.component(__vue_component__.name, __vue_component__);
-        Vue.component(__vue_component__$1.name, __vue_component__$1);
-        Vue.component(__vue_component__$2.name, __vue_component__$2);
+      Vue.component(script.name, script);
+      Vue.component(script$1.name, script$1);
+      Vue.component(script$2.name, script$2);
     }
-};
+  };
 
-module.exports = Esnek;
+  return Esnek;
+
+}(vue, changeCase));
+//# sourceMappingURL=esnek.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,38 @@
 import vue from 'rollup-plugin-vue'
 
-export default {
-  input: 'src/esnek.js',
-  output: [
-    {
-      file: 'dist/js/esnek.js',
-      format: 'cjs'
-    }
-  ],
-  plugins: [
-    vue()
-  ]
-};
+const FILE_NAME = "esnek";
+const sourcemap = true;
+
+export default [
+  {
+    input: 'src/esnek.js',
+    output: [
+      {
+        file: `dist/${FILE_NAME}.common.js`,
+        format: 'cjs',
+        sourcemap
+      },
+      {
+        file: `dist/${FILE_NAME}.es.js`,
+        format: 'es',
+        sourcemap
+      }
+    ],
+    plugins: [
+      vue()
+    ]
+  },
+  {
+    input: 'src/esnek.js',
+    output: [
+      {
+        file: `dist/${FILE_NAME}.js`,
+        format: 'iife',
+        sourcemap
+      },
+    ],
+    plugins: [
+      vue()
+    ]
+  }
+];
